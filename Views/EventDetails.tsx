b14d61598e7da7504b54b8bb5d99e472442a19e9
Joni Juntto <you@example.com>
2022-06-13 01:13:52 +0300

    Add users to event ready

--- Views/EventDetails.tsx
+++ Views/EventDetails.tsx
@@ -1,13 +1,45 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import {useAppSelector} from '../hooks';
+import {useTheme} from '@react-navigation/native';
 
 export default ({navigation}: any): JSX.Element => {
+  const {colors} = useTheme();
   const event = useAppSelector(state => state.event.value);
+  const user = useAppSelector(state => state.user.value);
   console.log(event);
+
+  const attendToEvent = async () => {
+    try {
+      const url =
+        'https://hlw2l5zrpk.execute-api.eu-north-1.amazonaws.com/dev/add-post-to-user/' +
+        user.uid +
+        '/' +
+        event.id;
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          userId: user.uid,
+          username: user.username,
+          photo: user.photo,
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <View>
       <Text>{event?.name}</Text>
+      <TouchableOpacity onPress={() => attendToEvent()}>
+        <Text style={{color: 'red'}}>Attend to event</Text>
+      </TouchableOpacity>
     </View>
   );
 };

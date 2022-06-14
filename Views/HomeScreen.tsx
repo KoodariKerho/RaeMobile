4a0d3c1abedb2157482795f972b8029eb92f7f8a
Joni Juntto <you@example.com>
2022-06-14 21:19:35 +0300

    Added photo to event

--- Views/HomeScreen.tsx
+++ Views/HomeScreen.tsx
@@ -1,4 +1,13 @@
-import {View, TouchableOpacity, Image, StyleSheet} from 'react-native';
+import {
+  View,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+  SafeAreaView,
+  FlatList,
+  Dimensions,
+  Button,
+} from 'react-native';
 import React, {useState, useEffect} from 'react';
 import {useAppSelector} from '../hooks';
 import {useAppDispatch} from '../hooks';
@@ -6,6 +15,7 @@ import {changeFriend} from '../features/friendSlice';
 import {Friend} from '../models/types';
 import {useTheme} from '@react-navigation/native';
 import Text from '../Components/CustomText';
+import {changeEvent} from '../features/eventSlice';
 
 export default ({navigation}: any): JSX.Element => {
   const dispatch = useAppDispatch();
@@ -13,14 +23,8 @@ export default ({navigation}: any): JSX.Element => {
   const [friendsEvents, setFriendsEvents] = useState([]);
 
   const {colors} = useTheme();
-
-  //TODO: Make user login if not logged in
-  console.log(user);
-
   useEffect(() => {
-    //unmount function
     let unmounted = false;
-    //get friendlist
     const getFriendEvents = async () => {
       const url =
         'https://hlw2l5zrpk.execute-api.eu-north-1.amazonaws.com/dev/friends-events/' +
@@ -42,70 +46,123 @@ export default ({navigation}: any): JSX.Element => {
     };
   }, [user.uid]);
 
-  const addToOwnPosts = async (eventId: string) => {
-    const url =
-      'https://hlw2l5zrpk.execute-api.eu-north-1.amazonaws.com/dev/add-post-to-user/' +
-      user.uid +
-      '/' +
-      eventId;
-
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    const data = await response.json();
-    console.log(data);
-    //TODO: Show toast error or success
-  };
-
   const goToFriendProfile = (friend: Friend) => {
     dispatch(changeFriend(friend));
     navigation.navigate('Friend');
   };
 
+  const goToEventDetails = event => {
+    console.log('THIS EVENT');
+    console.log(event);
+
+    dispatch(changeEvent(event));
+    navigation.navigate('Eventdetails');
+  };
+
+  const width = Dimensions.get('window').width;
+  const height = Dimensions.get('window').height;
+
+  const Item = ({post}) => {
+    const url = `https://portalvhdsp62n0yt356llm.blob.core.windows.net/bailataan-mediaitems/${post.event.mediaFilename}`;
+    console.log(url);
+    return (
+      <View
+        style={{
+          borderWidth: 1,
+          borderColor: '#ccc',
+          backgroundColor: colors.card,
+          borderRadius: 10,
+          width: width - 20,
+          margin: 10,
+          height: height * 0.2,
+          shadowColor: '#FFFFFF',
+          shadowOffset: {
+            width: 1,
+            height: 4,
+          },
+          shadowOpacity: 0.45,
+          shadowRadius: 3.84,
+
+          elevation: 5,
+        }}>
+        {/* author data */}
+        <View
+          style={{
+            position: 'relative',
+            left: 3,
+            top: 8,
+          }}>
+          <TouchableOpacity
+            onPress={() => goToFriendProfile(post.user.attribute_values)}>
+            <Image
+              style={{
+                width: 50,
+                height: 50,
+                borderRadius: 50,
+                borderWidth: 1,
+                borderColor: '#ccc',
+              }}
+              source={{uri: post.user.attribute_values.photo}}
+            />
+            <Text>{post.user.attribute_values.username}</Text>
+          </TouchableOpacity>
+        </View>
+        {/* Event data */}
+        <View>
+          <Text>{post.event.name}</Text>
+          <Text>{post.event.companyName}</Text>
+          <Text>{post.event.place}</Text>
+          <Image
+            style={{
+              width: 120,
+              height: 120,
+              borderRadius: 10,
+            }}
+            source={{
+              uri: url,
+            }}
+          />
+        </View>
+        <Button
+          onPress={() => goToEventDetails(post.event)}
+          title="Lisätietoja"
+        />
+      </View>
+    );
+  };
+
+  const renderItem = ({item}) => <Item post={item} />;
+
   return (
-    <View>
-      <TouchableOpacity onPress={() => navigation.navigate('Events')}>
-        <Text style={{color: 'red'}}>Events</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('QrReader')}>
-        <Text>HEERERRE</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('QR')}>
-        <Text>GENERATE QR</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('Friends')}>
-        <Text> FRIENDS</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
       <View>
-        {friendsEvents?.map((post: any) => {
-          console.log(post);
-          return (
-            <View>
-              <Text>Event id</Text>
-              <Text>{post.event.id}</Text>
-              <Text>User id</Text>
-              <Text style={{color: 'red'}}>
-                {post.user.attribute_values.id}
-              </Text>
-              <TouchableOpacity
-                onPress={() => goToFriendProfile(post.user.attribute_values)}>
-                {/* Add also friend data to profile */}
-                <Image
-                  style={{width: 50, height: 50}}
-                  source={{uri: post.user.attribute_values.photo}}
-                />
-              </TouchableOpacity>
-              <Text>ojfsoijoseifj</Text>
-              <TouchableOpacity onPress={() => addToOwnPosts(post.event.id)}>
-                <Text>Add to own</Text>
-              </TouchableOpacity>
-            </View>
-          );
-        })}
+        <SafeAreaView style={styles.container}>
+          <FlatList
+            data={friendsEvents}
+            renderItem={renderItem}
+            keyExtractor={item => item.id}
+          />
+        </SafeAreaView>
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
+
+/* {"event": {"availability": 100, "companyMediaFilename": null, "companyName": "Luxonia Events", 
+"dateActualFrom": "2022-08-13T21:00:00+03:00", "dateActualUntil": "2022-08-14T03:00:00+03:00", 
+"dateCreated": "2022-06-02T14:10:21Z", "datePublishFrom": "2022-06-08T08:00:00+03:00", "dateSalesFrom":
+ "2022-06-08T09:00:00+03:00", "dateSalesUntil": "2022-08-14T02:00:00+03:00", "favoritedTimes": 2, 
+ "hasFreeInventoryItems": false, "hasInventoryItems": true, "id": "6b65ef5e-d102-44ee-9769-ff734e5f119b", 
+ "isActual": false, "isFavorited": false, "isLong": false, "maxPrice": {"eur": 860}, "mediaFilename": "xl_51a940b692c59aacf3461d6eea73769ce39245e8bf03c7437fa7b432.jpeg",
+  "minPrice": {"eur": 860}, "name": "Club Luxonia 13.8.2022 K18", "place": "Pikku-Finlandia", "pricingInformation": null, 
+  "productType": 1, "salesEnded": false, "salesOngoing": true, "salesPaused": false, "salesStarted": true, "timeUntilActual": 5185874, 
+  '"timeUntilSalesStart": 0}, "user": {"attribute_values": {"email": "testiemail@gmail.xdlsd", "friends": [Array], "id": "MIFgceykluOyvTcVvGhKFTMIjMX2",
+   "photo": "https://firebasestorage.googleapis.com/v0/b/opiskelija-appi.appspot.com/o/logoteal.png?alt=media&token=a32ea342-c941-4e69-ab19-bcf02b3d4000", 
+   "posts": [Array], "username": "Huikka"}}} */
--- Views/Profile.tsx
+++ Views/Profile.tsx
@@ -1,13 +1,117 @@
-import {View} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Image,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import React, {useState} from 'react';
 import {useTheme} from '@react-navigation/native';
 import Text from '../Components/CustomText';
+import {useAppSelector} from '../hooks';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 
 export default (): JSX.Element => {
+  const user = useAppSelector(state => state.user.value);
+  const [username, setUsername] = useState(user.username);
+  const [photo, setPhoto] = useState({});
+  console.log(user);
+
+  const handleUploadPhoto = () => {
+    fetch(
+      'https://hlw2l5zrpk.execute-api.eu-north-1.amazonaws.com/dev/uploadfile',
+      {
+        method: 'POST',
+        body: createFormData({userId: '123'}),
+      },
+    )
+      .then(response => response.json())
+      .then(response => {
+        console.log('response', response);
+      })
+      .catch(error => {
+        console.log('error', error);
+      });
+  };
+
+  const createFormData = (body = {}) => {
+    const data = new FormData();
+
+    data.append('photo', {
+      name: photo.fileName,
+      type: photo.type,
+      uri: photo.uri,
+    });
+
+    Object.keys(body).forEach(key => {
+      data.append(key, body[key]);
+    });
+
+    return data;
+  };
+
+  const options = {
+    mediaType: 'photo',
+    quality: 1,
+    cameraType: 'back',
+    includeBase64: true,
+  };
+  const takePhoto = async () => {
+    const result = await launchCamera(options);
+    setPhoto(result);
+    handleUploadPhoto();
+    console.log(result);
+  };
+  const pickImage = async () => {
+    const result1 = await launchImageLibrary(options);
+    console.log(result1);
+  };
+
   const {colors} = useTheme();
   return (
     <View>
       <Text>Profile</Text>
+      <Image
+        source={{uri: user.photo}}
+        style={{width: 120, height: 120, borderRadius: 180}}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={e => setUsername(e)}
+        value={username}
+        placeholder="Username"
+        selectTextOnFocus={true}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: '#a0a1a3',
+    borderRadius: 20,
+    width: '80%',
+  },
+  fields: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: '20%',
+    height: '20%',
+    resizeMode: 'contain',
+  },
+  googleButton: {
+    marginVertical: 20,
+    marginHorizontal: 20,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+});
